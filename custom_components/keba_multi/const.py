6065139c07abb1ff3f2267948e9e571255ae22ee
Next04 <183516849+next04-ha@users.noreply.github.com>
2025-12-23 16:55:15 +0100

    Update const.py

--- custom_components/keba_multi/const.py
+++ custom_components/keba_multi/const.py
@@ -3,8 +3,9 @@ DOMAIN = "keba_multi"
 
 # Configuration keys for user input
 CONF_HOST = "host"
-CONF_RFID = "rfid"  # Aggiungi questa riga
+CONF_RFID = "rfid"
 CONF_FS_INTERVAL = "refresh_interval"
+CONF_SCAN_INTERVAL = "scan_interval"  # <-- Aggiungi questa riga
 
 DEFAULT_RFID = "00845500"
 DEFAULT_SCAN_INTERVAL = 5
